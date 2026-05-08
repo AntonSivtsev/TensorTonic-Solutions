1f1acc62b5160deb266003e463d22d6e809e8724
AntonSivtsev <136502764+AntonSivtsev@users.noreply.github.com>
2026-05-08 08:10:51 +0300

    Add solution for la-cosine-similarity

---
+++ study-plans/math-linear-algebra/la-cosine-similarity/la-cosine-similarity.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def cosine_similarity(a, b):
+    """
+    Returns: float in [-1, 1], cosine similarity between a and b.
+    """
+    a = np.array(a, dtype=np.float64)
+    b = np.array(b, dtype=np.float64)
+    norm_a = np.linalg.norm(a)
+    norm_b = np.linalg.norm(b)
+    if norm_a != 0 and norm_b != 0:
+        x = sum(a * b) / (norm_a * norm_b)
+    else:
+        x = 0
+
+    return x
